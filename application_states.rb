297dbd86ec97e2f100d1bb3be5584502b74f09e8
Vishal Patel <vppatel@ualberta.ca>
2019-03-18 15:45:47 -0600

    The last of the contracts
    Is also the best of the contracts.

--- application_states.rb
+++ application_states.rb
@@ -7,9 +7,9 @@ require_relative 'qt_application'
 # from, do we need contracts for it?
 
 # Base application state, defines and creates the window here.
-class ApplicationState
+class ApplicationStateMachine
   include StatePattern
-  # attr_accessor :window
+  attr_accessor :window
   set_initial_state(TitleScreenState)
 
   def is_valid?
@@ -24,19 +24,38 @@ class ApplicationState
     # Use a singleton to create the QT GUI.
     @window = QTApplication.instance
 
+    is_valid?
+  end
+
+
+end
+
+class GameApplication
+
+  def is_valid?
+    assert @window.height > 0
+    assert @window.width > 0
+    assert @window.visible
+    assert @window.is_a? QTApplication
+    assert @state_machine.is_a? ApplicationStateMachine
+  end
+
+  # Create GUI here.
+  def initialize
+    # Use a singleton to create the QT GUI.
+    @window = QTApplication.instance
+    @state_machine = ApplicationStateMachine.new
+
+    # Setup "connections" for QT to capture mouse clicks.
+
     # Display the window
 
     is_valid?
   end
 
-  # Do we need a callback to handle user interaction?
-  def call_back
-
-  end
-
   # Main application loop.
   def main
-
+    is_valid?
     # On launch.
     # Display title screen.
     # If settings button clicked from title screen.
@@ -45,6 +64,11 @@ class ApplicationState
     # Open game screen.
     # When game ends.
     # Open title screen.
+    is_valid?
+  end
+
+  # This gets called whenever a user clicks.
+  def call_back
 
   end
 
@@ -52,8 +76,17 @@ end
 
 class TitleScreenState < StatePattern::State
 
+  def is_valid?
+    assert @window.height > 0
+    assert @window.width > 0
+    assert @window.visible
+    assert @window.is_a? QTApplication
+  end
+
   def enter
+    is_valid?
     # assert class invariants?
+    is_valid?
   end
 
   def open_settings
@@ -68,8 +101,17 @@ end
 
 class SettingsScreenState < StatePattern::State
 
+  def is_valid?
+    assert @window.height > 0
+    assert @window.width > 0
+    assert @window.visible
+    assert @window.is_a? QTApplication
+  end
+
   def enter
+    is_valid?
     # assert class invariants?
+    is_valid?
   end
 
   def open_title_screen
@@ -80,8 +122,17 @@ end
 
 class GameScreenState < StatePattern::State
 
+  def is_valid?
+    assert @window.height > 0
+    assert @window.width > 0
+    assert @window.visible
+    assert @window.is_a? QTApplication
+  end
+
   def enter
+    is_valid?
     # Add the assertions from the game as before.
+    is_valid?
   end
 
   def open_title_screen
