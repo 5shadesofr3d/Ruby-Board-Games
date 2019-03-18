94b4b6b3a7523f31d0b5ca00c84ca5d69a2779e0
joseram97 <jose@ualberta.ca>
2019-03-18 14:56:01 -0600

    Added the bodies for the player files

--- ai_player.rb
+++ ai_player.rb
@@ -1,1 +1,11 @@
 # This is the ai player that is available if the local player can play
+require 'test/unit'
+require_relative 'player_abstract'
+
+class AIPlayer < Player
+
+	def get_move
+		# This will return the move that the player will take. This will come
+		# from a move generator for the AI
+	end
+end
--- local_player.rb
+++ local_player.rb
@@ -1,1 +1,12 @@
 # This is the local player object. It is inherited from the player abstract class
+require 'test/unit'
+require_relative 'player_abstract'
+
+class LocalPlayer < Player
+
+	def get_move
+		# This is going to get the input from the keyboard from what the player should
+		# be doing
+		
+	end
+end
