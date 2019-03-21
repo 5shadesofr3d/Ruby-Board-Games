e55b006ea58ace57257fd2aeac80bdbce91e11a1
curtgg <cgoud@ualberta.ca>
2019-03-21 16:49:29 -0600

    blue screen

--- gui/title/title.rb
+++ gui/title/title.rb
@@ -9,14 +9,17 @@ class Title < Qt::Widget
     assert height.is_a? Numeric
     assert width > 0
     assert height > 0
+    super()
 
     @layout = Qt::GridLayout.new(self)
 		@layout.setSpacing(10)
     setLayout(@layout)
-
     setScreenSize(width,height)
-
+    setWindowTitle("Ruby-Board-Games")
+    move(100, 100)
+    setBackground(Qt::darkCyan)
     drawMenu()
+    show()
   end
 
   def setScreenSize(width, height)
@@ -29,7 +32,17 @@ class Title < Qt::Widget
 		assert height() == height
   end
 
+  def setBackground(c = Qt::white)
+    assert c.is_a? Qt::Enum or c.is_a? Qt::Color
+    palette = Qt::Palette.new(c)
+		setAutoFillBackground(true)
+		setPalette(palette)
+    assert palette.is_a? Qt::Palette
+  end
+
   def drawMenu
+    assert valid?
+
     assert valid?
   end
 
@@ -37,7 +50,12 @@ class Title < Qt::Widget
     return false unless width().is_a?(Integer) and width().between?(100, 1920)
     return false unless height().is_a?(Integer) and height().between?(100, 1080)
     return false unless @layout.is_a? Qt::GridLayout
+    return true
   end
 
 
 end
+
+app = Qt::Application.new ARGV
+t = Title.new
+app.exec
