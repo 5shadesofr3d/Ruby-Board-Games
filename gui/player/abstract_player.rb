6f3087fa1a3e2f65e42fe65b4b90148749efdb32
curtis goud <cgoud@ualberta.ca>
2019-03-23 16:35:21 -0600

    abstract player contracts

--- gui/player/abstract_player.rb
+++ gui/player/abstract_player.rb
@@ -10,7 +10,7 @@ class Player < Qt::Object
 
 	attr_reader :name, :color, :current_chip
 	attr_accessor :wins, :losses, :ties, :game, :current_column
-	
+
 	slots :enable, :disable
 	signals :finished
 
@@ -20,6 +20,8 @@ class Player < Qt::Object
 		# Set the name of the player and make the score 0
 		#pre
 		assert player_name.is_a? String
+		#TODO: Player color
+
 
 		@name = player_name
 		@wins = 0
@@ -28,6 +30,12 @@ class Player < Qt::Object
 		@color = Qt::Color.new(player_color)
 
 		#post
+		assert @color.is_a? Qt::Color
+		assert @name.is_a? String
+		assert @name == player_name
+		assert @wins >= 0 and @wins.is_a? Integer
+		assert @losses >= 0 and @losses.is_a? Integer
+		assert @ties >=0 and @ties.is_a? Integer
 		assert valid?
 	end
 
@@ -38,11 +46,13 @@ class Player < Qt::Object
 		return false unless ties.is_a?(Integer) and @ties >= 0
 		return false unless color.is_a?(Qt::Color)
 		return false unless game.is_a?(Game) or game == nil
-
 		return true
 	end
 
 	def left()
+		assert game.is_a? Game
+		assert model.is_a? BoardModel
+
 		return if current_column == 0
 
 		model = game.board.model
@@ -51,11 +61,18 @@ class Player < Qt::Object
 			from: model.head(current_column),
 			to: model.head(current_column - 1),
 			time: 100)
-		
+
 		@current_column -= 1
+
+		assert @current_column.is_a? Integer
+		assert game.is_a? Game
+		assert model.is_a? BoardModel
 	end
 
 	def right()
+		assert game.is_a? Game
+		assert model.is_a? BoardModel
+
 		return if current_column == game.board.model.columns.max
 
 		model = game.board.model
@@ -64,26 +81,50 @@ class Player < Qt::Object
 			from: model.head(current_column),
 			to: model.head(current_column + 1),
 			time: 100)
-		
+
 		@current_column += 1
+
+		assert @current_column.is_a? Integer
+		assert game.is_a? Game
+		assert model.is_a? BoardModel
 	end
 
 	def drop()
+		assert game.is_a? Game
+		assert current_column.is_a? Integer
+		assert current_column.is_a? >= 0
+		assert current_chip.is_a? BoardChip
+
 		game.board.drop(current_chip, current_column)
 		@current_chip = nil
+
+		assert @current_chip == nil
 	end
 
 	def enable()
+		assert game.is_a? Game
+		assert game.board.is_a? Board
+
 		@current_chip = game.constructChip(color)
 		@current_column = 0
 		connect(game.board, SIGNAL("dropped()"), self, SIGNAL("finished()"))
+
+		assert @current_chip.is_a? BoardChip
+		assert current_column.is_a? Integer and current_column >= 0
 	end
 
 	def disable()
+		assert game.board.is_a? Board
+
 		disconnect(game.board, SIGNAL("dropped()"), self, SIGNAL("finished()"))
 	end
 
 	def total_score
+		assert wins.is_a? Integer
+		assert losses.is_a? Integer
+		assert wins >= 0
+		assert losses >= 0
+
 		return wins - losses
 	end
 
